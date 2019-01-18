6589a758f49fd595894f76c0c567b8fee152b05b
Wolfman13 <34652519+Wolfman13@users.noreply.github.com>
2019-01-18 15:33:55 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,81 @@
 extern crate rand;
 #[macro_use]
 extern crate text_io;
+extern crate termion;
 
 use rand::prelude::*;
+use std::io;
+use std::cmp::Ordering;
+use termion::{color, clear};
+
+//reset = color::Fg(color::Reset)
 
 fn main() {
-    println!("Welcome to The Game");
-    println!("What is your name player?");
+    println!("{}", clear::All);
+
+    println!("{green}Welcome to The Game
+        \nWhat is your name player?",
+        green = color::Fg(color::Green));
     let player_name: String = read!("{}\n");
 
-    println!("Ok, {}! The rules are simple.", player_name);
-    println!("Guess a number between 0 and 100.");
-    println!("If you guess correctly you win!");
+    println!("Ok, {red}{name}{green}! The rules are simple.
+        \nGuess a number between 0 and 100.
+        \nIf you guess correctly you win!",
+        red = color::Fg(color::Red),
+        green = color::Fg(color::Green),
+        name = player_name);
 
-    println!("Are you ready? (Y/N)");
-    let pregame_answer: String = read!("{}\n");
+    loop {
+        println!("Are you ready? (Y/N)");
+        let pregame_answer_lowercase: String = read!("{}\n");
+        let pregame_answer = pregame_answer_lowercase.to_uppercase();
 
-    if pregame_answer == "Y" || pregame_answer == "y" {
-        loop {
-            let mut rng = thread_rng();
-            let random_num = rng.gen_range(0, 101);
-            println!("So go on... Guess");
 
+        if pregame_answer == "Y" {
             loop {
+                let mut rng = thread_rng();
+                let random_num = rng.gen_range(0, 101);
+                println!("So go on... Guess");
 
-                let player_num: i32 = read!("{}\n");
+                loop {
+                    let mut guess = String::new();
+                    io::stdin().read_line(&mut guess)
+                        .expect("Failed to read line");
+                    let guess: u32 = match guess.trim().parse() {
+                        Ok(num) => num,
+                        Err(_) => {
+                            println!("{blue}Please use only numbers{green}",
+                                blue = color::Fg(color::Blue),
+                                green = color::Fg(color::Green));
+                            continue;
+                        },
+                    };
 
-                if player_num > random_num {
-                    println!("The number I was thinking of is a lil lower");
-                } else if player_num < random_num {
-                    println!("The number I was thinking of is a lil higher");
-                } else {
-                    println!("Well done you got it right, the random number is: {}", random_num);
-                    break;
+                    match guess.cmp(&random_num) {
+                        Ordering::Less => println!("The number I was thinking of is a lil higher"),
+                        Ordering::Greater => println!("The number I was thinking of is a lil lower"),
+                        Ordering::Equal => {
+                            println!("{blue}Well done you got it right!{green}",
+                                blue = color::Fg(color::Blue),
+                                green = color::Fg(color::Green));
+                            break;
+                        }
+                    }
                 }
-            }
 
-            println!("Wanna play another round? (Y/N)");
-            let player_choice: String = read!("{}\n");
-
-            if player_choice == "Y" || player_choice == "y" {
-                println!("Yeay!");
-            } else {
                 break;
             }
+        } else if pregame_answer == "N" {
+            println!("Maybe next time then");
+            break;
+        } else {
+            println!("{blue}Please enter either 'Y' for yes or 'N' for no{green}",
+                blue = color::Fg(color::Blue),
+                green = color::Fg(color::Green));
         }
     }
-    println!("I'm sad to see you leave, bye");
+
+    println!("{reset}{clear}",
+        reset = color::Fg(color::Reset),
+        clear = clear::All);
 }
